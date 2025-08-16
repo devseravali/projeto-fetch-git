408f3ceae037e55169af7f9cce3c77456591f7b0
devseravali <devseravali@gmail.com>
2025-08-15 22:33:23 -0300

    Remoção de variables.js em excesso

--- src/js/services/variables.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://api.github.com/users'
-const repositoriesQuantity = 10
-const baseFollowersUrl = 'https://api.github.com/users/{username}/followers'
-const baseFollowingsUrl = 'https://api.github.com/users/{username}/following'
-const baseEvents = 'https://api.github.com/users/{username}/events'
-
-
-export { baseUrl, repositoriesQuantity, baseFollowersUrl, baseFollowingsUrl, baseEvents }
